9ab60529b76e1a6b1c42785e59606e51e4a9fde9
RogerioAlexandreBraga <rogerioabraga@hotmail.com>
2024-09-17 15:14:44 -0300

    Finalização

--- movies_lib/src/main.jsx
+++ movies_lib/src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from './App.jsx';
-import Home from './pages/Home.jsx';
-import Movie from './pages/Movie.jsx';
-import Search from './pages/Search.jsx';
+import App from "./App";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Search from "./pages/Search";
 
-import './index.css';
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
